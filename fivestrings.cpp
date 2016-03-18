eb17edd59d2e81e88c61e6e1c5e11bdf047047ca
darkskeeper <dartskeeper@mail.ru>
2016-03-18 14:47:45 +0300

    fivestrings.cpp fixed

--- fivestrings.cpp
+++ fivestrings.cpp
@@ -5,44 +5,45 @@ using namespace std;
 
 int main()
 {
+    const int Ace = 12, King = 11, Queen = 10, Jacket = 9, Ten = 8, Five = 3, Ncards_one_suit = 13, Ncards_hand = 5;
     int Ntests;
     cin >> Ntests;
-    while( (Ntests--) != 0 )
+    while( Ntests-- )
     {
         vector <int> cards(52, 0);
         vector <int> combs(5, 0);
-        vector <int> your_cards(5, 0);
+        vector <int> your_cards(Ncards_hand, 0);
         string *a;
-        a = new string[5];
-        for ( int i = 0 ; i < 5 ; i++ )
+        a = new string[Ncards_hand];
+        for ( int i = 0 ; i < Ncards_hand ; i++ )
         {
             cin >> a[i];
         }
         int rank, suit;
-        for ( int i = 0 ; i < 5 ; i++ )
+        for ( int i = 0 ; i < Ncards_hand ; i++ )
         {
-            if(a[i].at(0)<='9'&&a[i].at(0)>='2')
+            if ( a[i].at(0) <= '9' && a[i].at(0) >= '2' )
             {
-                rank = a[i].at(0) - '2';
+                rank = a[i].at(0) - '2';//переводим char в int, где rank будет значение (карты - 2), то есть для 5 rank = 3
             }
             else
             {
                 switch(a[i].at(0))
                 {
                 case 'A':
-                    rank = 12;
+                    rank = Ace;
                     break;
                 case 'J':
-                    rank = 9;
+                    rank = Jacket;
                     break;
                 case 'K':
-                    rank = 11;
+                    rank = King;
                     break;
                 case 'Q':
-                    rank = 10;
+                    rank = Queen;
                     break;
                 case 'T':
-                    rank = 8;
+                    rank = Ten;
                     break;
                 }
             }
@@ -61,38 +62,37 @@ int main()
                 suit = 3;
                 break;
             }
-            cards [rank + 13 * suit] = 1;
-            your_cards[i] = rank + 13 * suit;
+            your_cards[i] = rank + Ncards_one_suit * suit;
+            cards [your_cards[i]] = 1;
         }
-        for (int i = 0; i < 13; i++)
+        for (int i = 0; i < Ncards_one_suit; i++)
         {
-            combs[ cards[i] + cards[i + ( 1 * 13 ) ] + cards[i + ( 2 * 13 ) ] + cards[i + ( 3 * 13 ) ] ]++;
+            combs[cards[i] + cards[i + (1 * Ncards_one_suit )] + cards[i + (2 * Ncards_one_suit )] + cards[i + (3 * Ncards_one_suit )] ]++;
         }
         while(true)
         {
-            if( combs[1] == 5 )
+            if( combs[1] == Ncards_hand )
             {
                 bool is_flash = true;
-                int your_suit = your_cards[0]/13;
+                int your_suit = your_cards[0]/Ncards_one_suit;
                 bool is_straight;
-                int min = your_cards[0]%13;
+                int min = your_cards[0]%Ncards_one_suit;
                 int max = min;
-                for( int i = 1 ; i < 5 ; i++ )
+                for( int i = 1 ; i < Ncards_hand ; i++ )
                 {
-                    if( your_cards[i] / 13 != your_suit && is_flash )
+                    if( your_cards[i] / Ncards_one_suit != your_suit && is_flash )
                     {
                         is_flash = false;
                     }
-                    if( your_cards[i] % 13 > max )
+                    if( your_cards[i] % Ncards_one_suit > max )
                     {
-                        max = your_cards[i]%13;
+                        max = your_cards[i] % Ncards_one_suit;
                     }
-                    if ( your_cards[i] % 13 < min )
+                    if ( your_cards[i] % Ncards_one_suit < min )
                     {
-                        min = your_cards[i]%13;
+                        min = your_cards[i] % Ncards_one_suit;
                     }
                 }
-                
                 if(max-min == 4)
                 {
                     is_straight = true;
@@ -100,17 +100,17 @@ int main()
                 else
                 {
                     is_straight = false;
-                    if ( max == 12 )
+                    if ( max == Ace )
                     {
                         max = 0;
-                        for ( int i = 1 ; i < 5 ; i++ )
+                        for ( int i = 0 ; i < Ncards_hand ; i++ )
                         {
-                            if( your_cards[i] % 13 > max && your_cards[i] % 13 != 12)
+                            if( your_cards[i] % Ncards_one_suit > max && your_cards[i] % Ncards_one_suit != Ace)
                             {
-                                max = your_cards[i]%13;
+                                max = your_cards[i] % Ncards_one_suit;
                             }
                         }
-                        if ( max == 3 )
+                        if ( max == Five )
                         {
                             is_straight = true;
                         }
@@ -126,7 +126,7 @@ int main()
                     {
                         if (is_flash == true)
                         {
-                            if (max == 12)
+                            if (max == Ace)
                             {
                                 cout << "royal flush";
                             }
@@ -175,5 +175,6 @@ int main()
         }
         cout << endl;
     }
+    system("pause");
     return 0;
 }
